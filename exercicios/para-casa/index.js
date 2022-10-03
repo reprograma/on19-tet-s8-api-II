4fd3663b5378a5124c71dbfdc009dd2b9f4852f0
Shirlene Teles <shiteles@gmail.com>
2022-10-03 17:09:58 -0300

    resolução exercico get e post

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,45 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+const port = 3030
+const listaDeProdutos = require("./model/produtos.json")
+
+app.use(express.json())
+
+// para ver todos os produtos http://localhost:3030/produtos/
+
+app.get("/produtos/filtro",(req,res)=>{
+    const filtraPorNome = req.query.nome?.toLowerCase() //filtra por nome http://localhost:3030/produtos/filtro?nome=air
+    const FiltraPorValor = req.query.valor // filtra por valor http://localhost:3030/produtos/filtro?valor=719
+    const filtraPorDescricao = req.query.descricao?.toLowerCase() //filtra por descrição http://localhost:3030/produtos/filtro?descricao=caro
+    
+    const produtoSElecionado = listaDeProdutos.filter((produto,i) => {
+        if(filtraPorNome) {
+            return produto.nome.toLowerCase().includes(filtraPorNome)
+        }
+        if (FiltraPorValor) {
+            return produto.valor == FiltraPorValor
+        }
+        if(filtraPorDescricao) {
+            return produto.descricao.toLowerCase().includes(filtraPorDescricao)
+        }
+        return produto
+    })
+    res.json(produtoSElecionado)
+})
+
+app.get("/produtos/:id",(req,res)=>{
+    const id = req.params.id // filtra por ID http://localhost:3030/produtos/3
+    const produtoSelecionado = listaDeProdutos.filter((item, index)=> item.id == id)
+        res.json(produtoSelecionado)
+})
+
+app.post("/produtos/add",(req,res)=>{
+    const body = req.body  // para adicionar produto http://localhost:3030/produtos/add
+    listaDeProdutos.push(body)
+    res.send(listaDeProdutos)
+})
+
+app.listen(port,()=>{
+    console.log("Api está escutando na porta 3030");
+})
---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,56 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+const port = 3000
+const listaDeFilmes = require("./model/filmes-lista.json")
+
+app.use(express.json())
+
+app.get("/",(req, res)=>{
+    res.send("Hello World")
+})
+
+
+app.get("/filmes",(req,res)=>{
+    const filtroNome = req.query.nome
+    const filtroAno = req.query.ano
+
+    const filmeEscolhido = listaDeFilmes.filter((item,index) => {
+
+        if(filtroNome) {
+            return item.Title.toLowerCase() === filtroNome.toLocaleLowerCase()
+        }
+        if (filtroAno) {
+            return item.Year === filtroAno
+        }
+        return item
+    })
+
+    res.json(filmeEscolhido)
+})
+
+app.get("/filmes/:id",(req,res)=>{
+    const id = req.params.id
+
+    const filmeEscolhido = listaDeFilmes.filter((item, index)=> item.id == id)
+        res.json(filmeEscolhido)
+  
+})
+
+app.post("/filmes",(req,res)=>{
+    const body = req.body
+
+    listaDeFilmes.push(body)
+
+    res.send(listaDeFilmes)
+})
+
+app.listen(port,()=>{
+    console.log("Api está escutando na porta 3000");
+})
+
+/*
+HOST = LOCAL HOST
+PORT = 3000 (PORTA)
+http://host:port  = http://localhost:3000/
+*/ 
