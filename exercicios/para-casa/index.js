eb2e8d3e3effb041a375640c36f73180d5a31b95
elvirabl <elvirabrunoleme@gmail.com>
2022-10-05 18:44:33 -0300

    Elvira Bruno - exercício semana 8 Reprograma

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,46 @@
+const { response } = require("express")//require só funciona para bibliotecas, no caso a biblioteca express
+const express = require("express")
+const app = express()//criando uma função dentro de app
+const port = 3333//declarar um valor qualquer
+const listaDeProdutos = require("./model/produtos.json")
+
+app.use(express.json())
+//http://localhost:3333/produto localiza a lista de produtos
+app.get("/produto",(req,res)=>{// app chama uma get, get recebe 2 argumentos... 1 argumento o end point(no caso /), 2 trabalha a função call bak e o argumento 1
+    const filtroNome = req.query.nome?.toLocaleLowerCase() //filtro por nome
+    const filtroDescricao = req.query.descricao?.toLocaleLowerCase() //filtro pro descrição
+
+
+    const produtoEscolhido = listaDeProdutos.filter((item,index) => {
+
+        if(filtroNome) {
+            return item.Title.toLowerCase() === filtroNome.toLocaleLowerCase()
+        }
+        if (filtroDescricao) {
+            return item.descricao === filtroDescricao
+        }
+        return item
+    })
+
+    res.json(produtoEscolhido)
+})
+
+app.get("/produto/:id",(req,res)=>{//consulta refinada usando params http://localhost:3333/produto/4
+    const id = req.params.id //filtro por id
+console.log(req.params)
+    const produtoEscolhido = listaDeProdutos.filter((item, index)=> item.id == id)
+        res.json(produtoEscolhido)
+  
+})
+
+app.post("/produto",(req,res)=>{
+    const body = req.body
+
+    listaDeProdutos.push(body)
+
+    res.send(listaDeProdutos)//send é um método
+})
+// manipula questão e filtrar a resposta ideal
+app.listen(port, () =>{//callbak só pra imprimir // listen 
+    console.log("Api está escutando na porta 3333");
+})//listen = ouvindo quando a porta 3333 for solicitada
---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,51 @@
+//segunda parte sábados
+const { response } = require("express")
+const express = require("express")
+const app = express()
+const port = 3000
+const listaDeFilmes = require("./model/filmes-lista.json")
+
+app.use(express.json())
+
+app.get("/",(req, res)=>{
+    res.send("Hello World")
+})
+
+
+app.get("/filmes",(req,res)=>{
+    const filtroNome = req.query.nome
+    const filtroAno = req.query.nome
+
+    const filmeEscolhido = listaDeFilmes.filter((item,index) => {
+
+        if(filtroNome) {
+            return item.Title.toLowerCase() === filtroNome.toLocaleLowerCase()
+        }
+        if (filtroAno) {
+            return item.Year === filtroAno
+        }
+        return item
+    })
+
+    res.json(filmeEscolhido)
+})
+
+app.get("/filmes/:id",(req,res)=>{
+    const id = req.params.id
+
+    const filmeEscolhido = listaDeFilmes.filter((item, index)=> item.id == id)
+        res.json(filmeEscolhido)
+  
+})
+
+app.post("/filmes",(req,res)=>{
+    const body = req.body
+
+    listaDeFilmes.push(body)
+
+    res.send(listaDeFilmes)
+})
+
+app.listen(port,()=>{
+    console.log("Api está escutando na porta 3000");
+})
