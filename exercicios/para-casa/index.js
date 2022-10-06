d97f4c7aa104cbc7cc7564d287665206fad45f6b
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-10-06 17:47:19 -0300

    route list all products done

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const listaDeProdutos = require("./model/produtos.json")
 
 app.get('/', (req, res) => {
   res.send('Hello World!, para casa, teste')
 })
+
+
+
+app.get("/produtos", (req, res) => {
+  res.json(listaDeProdutos)
+})
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
