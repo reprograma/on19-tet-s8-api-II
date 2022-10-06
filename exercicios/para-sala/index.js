f66a6514832468cf38f7815198ebc0a72aed4cc7
Lydianne Corrêa <lydianne.correa2@gmail.com>
2022-10-06 17:36:03 -0300

    erros corrigidos.

---
+++ exercicios/para-sala/index.js
@@ -0,0 +1,49 @@
+const { response } = require("express")
+const express = require("express")
+const app = express()
+const port = 3000
+const listaDeFilmes = require("./model/filmes-lista.json")
+
+app.use(express.json())
+
+app.get("/",(req, res)=>{
+    res.send("Hello World")
+})
+
+app.get("/filmes",(req,res)=>{
+    const filtroNome = req.query.nome
+    const filtroAno = req.query.nome
+
+    const filmeEscolhido = listaDeFilmes.filter((item,index) => {
+
+        if(filtroNome) {
+            return item.Title === filtroNome
+        }
+        if (filtroAno) {
+            return item.Year === filtroAno
+        }
+        return item
+    })
+
+    res.json(filmeEscolhido)
+})
+
+app.get("/filmes/:id",(req,res)=>{
+    const id = req.params.id
+
+    const filmeEscolhido = listaDeFilmes.filter((item, index)=> item.id == id)
+        res.json(filmeEscolhido)
+  
+})
+
+app.post("/",(req,res)=>{
+    const body = req.body
+
+    console.log(body)
+
+    res.send("usuario autenticado")
+})
+
+app.listen(port,()=>{
+    console.log("Api está escutando na porta 3000");
+})
