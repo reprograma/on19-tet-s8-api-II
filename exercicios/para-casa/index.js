825746a480c189dddbf30fb3e8339db530caf15b
Karina Carvalho <karina@blacknetsolucoes.com.br>
2022-10-06 17:51:38 -0300

    Exercicio para casa

---
+++ exercicios/para-casa/index.js
@@ -0,0 +1,38 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+const produtos = require("./model/produtos.json")
+
+app.use(express.json())
+
+app.get("/produtos/:id", (req, res) => {
+    const id = req.params.id // Retorna uma string
+
+    const produtoEscolhido = produtos.filter((item) => item.id == id)
+
+    res.json(produtoEscolhido)
+
+})
+
+app.get("/produtos", (req, res) => {
+
+    const nome = req.query.nome
+    const descricao = req.query.descricao
+    const produtoFiltro = produtos.filter((item) => {
+
+        if (nome) {
+            return item.nome.toLowerCase() === nome.toLowerCase()
+        }
+
+        if (descricao) {
+            return item.descricao.toLowerCase() === descricao.toLowerCase()
+        }
+        return item
+    })
+})
+
+
+app.listen(port, () => {
+    console.log(`Escutando na porta ${port}`)
+})
