68b2c92a0e7adf7d97107df2a953bac883eb6168
CibelleSena <cibellebh@hotmail.com>
2022-10-07 21:23:08 -0300

    Adicionando correção dos filtros aplicados

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,22 +1,18 @@
-// Requisitos do exercício:
-
 const express = require("express")
 const app = express()
 const port = 3000
 const listaDeProdutos= require("./model/produtos.json")
-
 app.use(express.json())
 
 //- [ ] criar uma rota GET que lista TODOS os produtos da lista de produtos.
-app.get("/produtos", (req, res) => {
-    res.json(listaDeProdutos)
+app.get("/produtos/:id", (req, res) => {
+     res.json(listaDeProdutos)
 })
 
 //- [ ] expor uma rota GET que recebe o ID de um produto e retorna apenas esse produto na lista de produtos
-app.get("/produtos/:id", (req, res) => {
+app.get("/produto/:id", (req, res) => {
     const id = req.params.id
-
-    const produtoEscolhido = listaDeProdutos.filter((item, index) => item.id == id)
+    const produtoEscolhido = listaDeProdutos.filter(item => item.id == id)
 
     res.json(produtoEscolhido)
 
@@ -34,24 +30,21 @@ app.post("/produtos", (req, res) => {
 //- [ ] Utilizar o recurso `req.params.query` para criar filtros ( ex.: buscar por nome do produto, valor... ) 
 //- * Os filtros ficam a seu critério mas espero que exista ao menos dois filtros para sua rota
 
-app.get("/produtos", (req, res) => {
+app.get("/produto:id", (req, res) => {
     const filtroNome = req.query.nome
     const filtroValor = req.query.valor
-    const filtroId = req.query.id
+    
 
-    const produtoEscolhido = listaDeProdutos.filter((item) => {
+    const produtoSelecionado = listaDeProdutos.filter((item) => {
         if(filtroNome) {
-            return item.nome.toLocaleLowerCase() === filtroNome.toLocaleLowerCase()
+            return item.nome.toLowerCase() == filtroNome.toLowerCase()
         }
         if(filtroValor) {
-            return item.valor === filtroValor
+            return item.valor == filtroValor
         }
-        if(filtroId) {
-            return item.id === filtroId
-        }
-        return item
+            return item
     })
-    res.json(produtoEscolhido)
+    res.json(produtoSelecionado)
 })
 
 
