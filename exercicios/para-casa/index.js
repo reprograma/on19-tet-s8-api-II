e33327f94136a281dbcab2ae3a09904aa479076c
liliatatyany <liliatatyany@gmail.com>
2022-10-25 21:43:30 -0300

    correções de código

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -4,17 +4,21 @@ const port = 3000
 const produtos = require("./model/produtos.json")
 app.use(express.json())
 
-app.get("/", (req, res) =>{
-    res.send("Hello World")
+
+//- [x] criar uma rota GET que lista TODOS os produtos da lista de produtos.
+app.get("/produtos", (req, res) =>{
+    res.send(produtos)
 })
 
-
+//- [x] expor uma rota GET que recebe o ID de um produto e retorna apenas esse produto na lista de produtos
 app.get("/produtos/:id", (req, res) => {
     const id = req.params.id
+    
     const prodEscolhido = produtos.filter((item, index) => item.id == id)
     res.json(prodEscolhido)
 })
 
+//[x] Utilizar o recurso `req.query` para criar filtros ( ex.: buscar por nome do produto, valor... ) - * Os filtros ficam a seu critério mas espero que exista ao menos dois filtros para sua rota * 
 app.get("/produtos", (req, res) => {
     const filtroNome = req.query.nome
     const filtroDescricao = req.query.descricao
@@ -24,7 +28,7 @@ app.get("/produtos", (req, res) => {
             return item.nome.toLowerCase() == filtroNome.toLowerCase()
         }
         if(filtroDescricao) {
-            return item.descricao.t == filtroDescricao
+            return item.descricao == filtroDescricao
         }
         return item
     })
@@ -32,10 +36,10 @@ app.get("/produtos", (req, res) => {
 })
 
 
+//[ ] Adicionar um novo item à lista de produtos e retorna a lista atualizada
 app.post("/produtos", (req, res) => {
     const body = req.body
     produtos.push(body)
-    console.log(body)
     res.json(produtos)
 })
 
