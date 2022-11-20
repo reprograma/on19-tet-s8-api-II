8c9218af6ed8cfb7c9f4433bddc666027ab532ae
gabrielliareias <gabrielliareias@gmail.com>
2022-11-20 13:53:11 -0300

    entrega de exercicios com a correção feita em aula

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -5,7 +5,7 @@ const port = 3000
 
 app.use(express.json())
 
-// TODO:[x] expor uma rota GET que recebe o ID de um produto e retorna apenas esse produto na lista de produtos - DONE
+// expor uma rota GET que recebe o ID de um produto e retorna apenas esse produto na lista de produtos - DONE
 app.get("/produto/:id",(req, res)=>{
     const id = req.params.id
     
@@ -13,8 +13,8 @@ app.get("/produto/:id",(req, res)=>{
 
     res.json(produtoEscolhido)
 })
-// TODO:[x] Utilizar o recurso `req.query` para criar filtros ( ex.: buscar por nome do produto, valor... ) - * Os filtros ficam a seu critério mas espero que exista ao menos dois filtros para sua rota * - DONE
-// TODO:[x] criar uma rota GET que lista TODOS os produtos da lista de produtos. - DONE
+//Utilizar o recurso `req.query` para criar filtros ( ex.: buscar por nome do produto, valor... ) - * Os filtros ficam a seu critério mas espero que exista ao menos dois filtros para sua rota * - DONE
+//criar uma rota GET que lista TODOS os produtos da lista de produtos. - DONE
 app.get("/produto",(req, res)=>{
     const filtroNome = req.query.name
     const filtroValor = req.query.valor
@@ -30,7 +30,7 @@ app.get("/produto",(req, res)=>{
     })
     res.json(produtosFiltrados)
 })
-// TODO:[x] Adicionar um novo item à lista de produtos e retorna a lista atualizada - DONE
+// Adicionar um novo item à lista de produtos e retorna a lista atualizada - DONE
 app.post("/produto",(req, res)=>{
     const body = req.body
 
