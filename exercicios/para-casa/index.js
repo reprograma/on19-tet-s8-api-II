a87f178446d8e5a1162d7021c9fe3eb21b5b50f6
judy <judysantos169@gmail.com>
2023-02-04 18:45:56 -0300

    updates

--- exercicios/para-casa/index.js
+++ exercicios/para-casa/index.js
@@ -1,19 +1,16 @@
-//O modelo dos produtos está em `/exercicios/para-casa/model/produtos.json
-const express = require("express")
+const  express = require("express")
 const app = express()
-const porta = 3001 // o ideal é usar uma porta que seja acima de 3 mil , mas por quê?!
+const porta = 3000 
 app.use(express.json())
-//importando produtos
+
 const listaDeProdutos = require("./model/produtos.json")
 
-app.get("/", (red, res)=>{
+app.get("/", (req, res)=>{
   res.send("Testando...")  //é esperando que essa mensagem exiba no terminal do postman
 })
 
 
-
 //- [ 1] expor uma rota GET que recebe o ID de um produto e retorna apenas esse produto na lista de produtos
-
 app.get("/produto/:id", (req, res)=> {
   const  id = req.params.id
   const produtoEscolhido = listaDeProdutos.filter((item, index)=> item.id ==id)
@@ -21,13 +18,13 @@ app.get("/produto/:id", (req, res)=> {
 })
 
 //- [ 2] criar uma rota GET que lista TODOS os produtos da lista de produtos.
-app.get("/produto", (req, res) => {
+app.get("/produtos", (req, res) => {
   res.json(listaDeProdutos)
    
  })
 //- [ 3] Adicionar um novo item à lista de produtos e retorna a lista atualizada
-app.post("/produto", (req, res) => {
-  const body = req.body  //mas por quê usar o body? e não outra palavra ?!
+app.post("/AddProduto", (req, res) => {
+  const body = req.body  
 
   listaDeProdutos.push(body)//adiciona item no final de uma arrya
 
@@ -59,19 +56,10 @@ app.get("/produto", (req, res) => {
 
  POST(postar):  é usado para enviar os dados aos recursos especificados. Ele solicita a url especificada postando os dados dados recebidos e retornando uma resposta como Future<>response.
 
-// usando o delete , para remover produto por Id, retorna uma array vazia[]
-//usando o get percebi que exibi somente o item q selecionei
-/*app.get("/produto/:id", (req, res)=>{
-  const body = req.body
-
-  listaDeProdutos.splice(body)//adiciona ou remove item de uma array
-
-  res.json(listaDeProdutos)
-
-})*/
+*/
 
 app.listen(porta, ()=>{
-  console.log("Aplicação funcionando !")// essa mensagem aparecerá no terminal do node
-})
+  console.log("Aplicação funcionando !")
+ })
 
 
